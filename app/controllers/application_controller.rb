78b320bd59e16655fd56d8a906a973d5db783158
Mr-Marksman <levshin.maximka@gmail.com>
2022-09-08 15:21:43 +0300

    Capabilities of device expanding, updating app appearance. Adding notices from bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
   	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :password, :password_confirmation, :current_password])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :events
-  resources :users, only: %i[show edit update]
+  resources :users, only: %i[show]
 end
