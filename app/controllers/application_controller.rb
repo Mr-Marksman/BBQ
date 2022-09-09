12f1c3f7f0b3dcab79012ff7a70365aecf40d4b4
Mr-Marksman <levshin.maximka@gmail.com>
2022-09-09 13:57:55 +0300

    Localisation, turbo-confirm added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
-
   def set_locale 
     I18n.locale = :ru
   end
--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module Bbq
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.i18n.load_path += Dir[Rails.root.join("lib", "locale", "*{rb,yml}")]
     config.i18n.default_locale = :ru
 
     # Configuration for the application, engines, and railties goes here.
