61be950ce17dbd1fa5817e0dac93b516417b42dd
Mr-Marksman <levshin.maximka@gmail.com>
2022-09-13 20:08:38 +0300

    forbidden to use already registered email to subscribe

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,7 +3,9 @@ class Subscription < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :user_name, presence: true
-  validates :user_email, presence: true
+  validates :user_email, presence: true, 
+    exclusion: { within: User.pluck(:email), message: I18n.t("validations.subscriptions.email_already_used") }
+
   validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
   validates :user_email, uniqueness: {scope: :event_id},  unless: -> { user.present? } 
   validate :not_event_creator
@@ -11,7 +13,7 @@ class Subscription < ApplicationRecord
 
   def not_event_creator
     if user == event.user
-      errors.add(:user_id, message: I18n.t('validations.subscriptions.self_subscription'))
+      errors.add(:user_id, message: I18n.t("validations.subscriptions.self_subscription"))
     end
   end
 
