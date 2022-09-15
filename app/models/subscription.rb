a9aaaaca6af546327b3f563c4563ba696f8b6f05
Mr-Marksman <levshin.maximka@gmail.com>
2022-09-16 00:13:41 +0300

    Fix subscription prohibition bug

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,19 +7,7 @@ class Subscription < ApplicationRecord
   validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
   validates :user_email, uniqueness: {scope: :event_id},  unless: -> { user.present? } 
   validate :not_event_creator
-  validate :user_not_registered
-  
-  def user_not_registered
-    if User.find_by(email: user_email).present?
-      errors.add(:user_email, message: I18n.t("validations.subscriptions.email_already_used"))
-    end
-  end
-
-  def not_event_creator
-    if user == event.user
-      errors.add(:user_id, message: I18n.t("validations.subscriptions.self_subscription"))
-    end
-  end
+  validate :user_not_registered, unless: -> { user.present? }
 
   def user_name 
     if user.present?
@@ -34,6 +22,20 @@ class Subscription < ApplicationRecord
       user.email
     else
       super
+    end
+  end
+
+  private
+
+  def user_not_registered
+    if User.find_by(email: user_email).present?
+      errors.add(:user_email, message: I18n.t("validations.subscriptions.email_already_used"))
+    end
+  end
+
+  def not_event_creator
+    if user == event.user
+      errors.add(:user_id, message: I18n.t("validations.subscriptions.self_subscription"))
     end
   end
 end
