19f7daf389e96264fc6d5c0382bb49e898535fea
Maxim Levshin <m.levshin@sftpro.ru>
2023-04-19 19:07:34 +0300

    Lightbox for pictures

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,17 @@
 module PhotosHelper
+  def next_photo_index(counter, picture_count)
+    if counter == picture_count
+      1
+    else
+      counter + 1
+    end
+  end
+
+  def prev_photo_index(counter, picture_count)
+    if counter == 1
+      picture_count
+    else
+      counter - 1
+    end
+  end
 end
